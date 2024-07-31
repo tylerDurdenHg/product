59df159f7b1605d75815aaa5316a053a9ac52560
tylerDurdenHg <tylerdurdenhg@hotmail.com>
2024-07-31 16:39:01 +0300

    Update ProductApplication.java
    sout was changed in github

--- src/main/java/com/hg/product/ProductApplication.java
+++ src/main/java/com/hg/product/ProductApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductApplication {
 
 	public static void main(String[] args) {
-		String formatted = "unusal text %s between %s".formatted("test1", "test2");
+		String formatted = "tylering another text %s between %s".formatted("test1", "test2");
 		System.out.println(formatted);
 		SpringApplication.run(ProductApplication.class, args);
 	}
